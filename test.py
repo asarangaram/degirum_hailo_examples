55d27b50192942808a3f588adc58972542f6404c
Ananda S RPi <4833102+asarangaram@users.noreply.github.com>
2025-08-14 12:04:56 +0530

    add zoo path

--- test.py
+++ test.py
@@ -28,7 +28,8 @@ if __name__ == "__main__":
         # Check supported devices
         try:
             supported_devices = dg.get_supported_devices(
-                inference_host_address="@local"
+                inference_host_address="@local",
+                zoo_url='models'
             )
         except Exception as e:
             print(f"Error fetching supported devices: {e}")
